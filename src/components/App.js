d7bf0b40db0d13bf525d63a7ef1273cf51459954
csuau <csuaubou@gmail.com>
2022-08-19 10:31:55 +0200

    add functions render list quote,add new quote

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,32 @@ import '../styles/App.scss';
 import quotes from '../data/quotes.json';
 import { useState } from 'react';
 
+
 function App () {
   const [data, setData] = useState(quotes);
+  const [newQuote, setNewQuote] = useState({ quote: "", character: "" })
 
-  const htmlData = data.map(quotes) => {
+  const htmlData = data.map(quotes => {
     return (<li>
-      <p>{quotes.quote}</p>
-      <p>{quotes.character}</p>
-    </li>)
+      <p>{quotes.quote} - <span>{quotes.character}</span></p>
 
+    </li>)
+  })
+
+  const handleNewQuote = ((ev) => {
+    setNewQuote({
+      ...newQuote, [ev.target.id]: ev.target.value
+    })
+    console.log(ev.target)
+  });
+
+  const handleClick = (ev) => {
+    ev.preventDefault();
+    setData([...data, newQuote]);
+    setNewQuote({ quote: "", character: "" })
   }
+
+
   return (
     <div className="App">
       <header>
@@ -22,6 +38,15 @@ function App () {
         <ul>
           {htmlData}
         </ul>
+        <form>
+          <label>Añadir una nueva frase</label>
+          <label htmlfor="quote">Frase</label>
+          <input type="text" name="quote" id="quote" onChange={handleNewQuote}></input>
+          <label htmlfor="character">Personaje</label>
+          <input type="text" name="character" id="character" onChange={handleNewQuote}></input>
+          <button type="submit" onClick={handleClick}>Añadir una nueva frase</button>
+        </form>
+
       </main>
 
     </div>
